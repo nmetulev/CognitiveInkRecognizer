56b8aa17a11f50aee15c066a03837247ad23a123
Nikola Metulev <nikolame@microsoft.com>
2017-08-01 16:44:03 -0700

    Add project files.

---
+++ shapre recognizer/MainPage.xaml.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Graphics.Canvas;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace shapre_recognizer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Inker.InkPresenter.InputDeviceTypes =
+            CoreInputDeviceTypes.Pen |
+            CoreInputDeviceTypes.Touch |
+            CoreInputDeviceTypes.Mouse;
+        }
+
+        public static byte[] ReadToEnd(System.IO.Stream stream)
+        {
+            long originalPosition = 0;
+
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            System.Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            System.Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    System.Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            button.IsEnabled = false;
+
+            var stream = await RenderImage();
+            
+            using (var client = new HttpClient())
+            {
+                var url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/dd105cca-c262-4d53-aa6d-69a3f8da8e9c/image?iterationId=979d32cd-5fc2-4707-9a6e-36d3c597af03";
+                client.DefaultRequestHeaders.Add("Prediction-Key", "39c59d59c944411d88eb9e4fd20e84ec");
+
+                var bytes = new byte[stream.Size];
+                var reader = new DataReader(stream.GetInputStreamAt(0));
+                await reader.LoadAsync((uint)(stream.Size));
+                reader.ReadBytes(bytes);
+
+
+                using (var content = new ByteArrayContent(bytes))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    var response = await client.PostAsync(url, content);
+                    dynamic predictionResponse = await response.Content.ReadAsStringAsync()
+                        .ContinueWith((readTask) => JsonConvert.DeserializeObject(readTask.Result));
+                    result.Text = predictionResponse.Predictions[0].Tag;
+                    resultPer.Text = (predictionResponse.Predictions[0].Probability.Value * 100).ToString();
+                }
+            }
+
+            button.IsEnabled = true;
+
+        }
+
+        private async Task<InMemoryRandomAccessStream> RenderImage()
+        {
+            CanvasDevice device = CanvasDevice.GetSharedDevice();
+            CanvasRenderTarget renderTarget = new CanvasRenderTarget(device, (int)Inker.ActualWidth, (int)Inker.ActualHeight, 96);
+
+            var stream = new InMemoryRandomAccessStream();
+
+            using (var ds = renderTarget.CreateDrawingSession())
+            {
+                ds.Clear(Colors.White);
+                ds.DrawInk(Inker.InkPresenter.StrokeContainer.GetStrokes());
+
+
+            }
+                await renderTarget.SaveAsync(stream, CanvasBitmapFileFormat.Png, 1f);
+
+
+            return stream;
+        }
+
+        private void ClearClicked(object sender, RoutedEventArgs e)
+        {
+            Inker.InkPresenter.StrokeContainer.Clear();
+        }
+    }
+}
